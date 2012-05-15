d99ac76d7083ecd9563e94c57ad8a050de04fc93
Eric <errkkgeorge@gmail.com>
2012-05-15 15:08:08 +0100

    Init

---
+++ index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="main">
+
+		<div class="row">
+			<section class="container wide">
+				<header>
+					<h1>SMS Sent</h1>
+				</header>
+				<div id="flip-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>Apps downloaded</h1>
+				</header>
+				<div id="app-counter" class="flip-counter"></div>
+			</section>
+		</div>
+
+		<div class="row">
+			<section class="container">
+				<header>
+					<h1>iPhones</h1>
+				</header>
+				<div id="iphones-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>iPads</h1>
+				</header>
+				<div id="ipads-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>Kindle Fires</h1>
+				</header>
+				<div id="kindle-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>Blackberries</h1>
+				</header>
+				<div id="bb-counter" class="flip-counter"></div>
+			</section>
+		</div>
+
+		<div class="row">
+			<section class="container">
+				<header>
+					<h1>Tweets</h1>
+				</header>
+				<div id="tweets-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>Instagram Pics</h1>
+				</header>
+				<div id="instagram-counter" class="flip-counter"></div>
+			</section>
+			<section class="container">
+				<header>
+					<h1>Instagram Signups</h1>
+				</header>
+				<div id="instagram_new-counter" class="flip-counter"></div>
+			</section>
+		</div>
+
+		<div class="ir" id="logo">RR</div>
+
+
+
+	</div>
+	<script src="js/libs/jquery-1.7.1.min.js"></script>
+	<script src="js/libs/counter.js"></script>
+	<script src="js/libs/masonry.js"></script>
+	<script src="js/libs/highcharts.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,76 @@
+$(function(){
+
+
+	function initCounter( name, persec ){
+		var inc = 1,
+		pace = 1 / persec * 1000;
+		
+		return new flipCounter( name + '-counter', {
+			value : 0,
+			pace : parseInt( 1 / persec * 1000 ),
+			inc: 1
+		});
+	}
+
+
+	var total = 8000000000000,
+	seconds_in_a_year = (365.25 * 24 * 60 * 60),
+	persec = Math.floor(total / seconds_in_a_year),
+	
+	apps_min = 13000,
+	apps_sec = ( apps_min / 60 ),
+	
+	iphones_min = 925,
+	iphones_sec = ( iphones_min / 60 ),
+	
+	ipads_min = 81,
+	ipads_sec = ( ipads_min / 60 ),
+	
+	kindle_min = 18,
+	kindle_sec = ( kindle_min / 60 ),
+	
+	bb_min = 103,
+	bb_sec = ( bb_min / 60 ), 
+	
+	instagram_sec = 58,
+	instagramnew_sec = 1,
+	tweets_sec = 1633,
+
+	counters = [];
+	
+	
+	
+
+	
+	counters.push( 
+		new flipCounter( 'flip' + '-counter', {
+			value : 0,
+			pace : 10,
+			inc: parseInt( persec / 100 )
+		}) 
+		);
+	
+	var app_pace = ( 1 / apps_sec * 10000 );
+	counters.push( 
+		new flipCounter( 'app' + '-counter', {
+			value : 0,
+			pace : app_pace,
+			inc: 10
+		}) 
+		);
+	counters.push( initCounter( 'iphones', iphones_sec ) );
+	counters.push( initCounter( 'ipads', ipads_sec ) );
+	counters.push( initCounter( 'kindle', kindle_sec ) );
+	counters.push( initCounter( 'bb', bb_sec ) );
+
+	counters.push( initCounter( 'tweets', tweets_sec ) );
+	counters.push( initCounter( 'instagram', instagram_sec ) );
+	counters.push( initCounter( 'instagram_new', instagramnew_sec ) );
+
+
+
+
+
+});
+
+
